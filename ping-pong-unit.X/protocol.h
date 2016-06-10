2659eec11bfecafe809c82db901742d462881731
Romain Mormont <r.mormont@student.ulg.ac.be>
2016-06-10 10:57:34 +0200

    add uart for pic30F

---
+++ ping-pong-unit.X/protocol.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   protocol.h
+ * Author: Romain
+ *
+ * Created on 10 juin 2016, 9:16
+ */
+
+#ifndef PROTOCOL_H
+#define	PROTOCOL_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* PROTOCOL_H */
+
